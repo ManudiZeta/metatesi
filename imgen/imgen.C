54c359a7026898d538f4ec778dbbdaccad258d64
Emanuele Zanusso <afteremanuele@gmail.com>
2026-01-01 19:37:52 +0100

    modifiche ordierne

--- imgen/imgen.C
+++ imgen/imgen.C
@@ -40,14 +40,14 @@ void una_grand ()
     delete c1;
     
     TH1D *histo1 = (TH1D*)gDirectory->Get("histo1");
-        
+    
     histo1->SetLineColor(kBlue);
     TString title_x = var + " [" + um + "]";
     histo1->GetXaxis()->SetTitle(title_x);
     histo1->GetYaxis()->SetTitle("counts []");
     TString title = "";
     histo1->SetTitle(title);
-            
+    
     TCanvas *tela = new TCanvas("tela", "tela");
     
     histo1->DrawCopy("HIST");
@@ -58,7 +58,7 @@ void una_grand ()
 }
 
 
-void due_grand()
+void due_grand(TString var, TString cut, TString um)
 {
     gStyle->SetOptStat(1);
     string in_1, in_2;
@@ -82,12 +82,17 @@ void due_grand()
     TTree *tree_2 = (TTree*)myf_1->Get("tree");
     
     TCanvas *c1 = new TCanvas("c1", "c1",800,600);
-    TString var_1 = "vpho_r_pRecoil";
-    TString var_2 = "nbar_mcP";
-    TString drawExpr1 = var_1 + ">>histo1(128,0,7)";
-    TString drawExpr2 = var_2 + ">>histo2(128,0,7)";
-    tree_1->Draw(drawExpr1, "nbar_mcPDG == -2112", "goff");
-    tree_2->Draw(drawExpr2, "nbar_mcPDG == -2112", "goff");
+    TString var_1 = "nbar_" + var;
+    TString var_2 = "nbar_" + var;
+    cout<<"var_1 = "<<var_1<<endl;
+    cout<<"var_2 = "<<var_2<<endl;
+    
+    TString drawExpr1 = var_1 + ">>histo1(256,0,-7)";
+    TString drawExpr2 = var_2 + ">>histo2(256,7,-7)";
+    TString cut_2 = "nbar_" + cut;
+    cout<<"cut_2 = "<<cut_2<<endl;
+    tree_1->Draw(drawExpr1, "", "goff");
+    tree_2->Draw(drawExpr2, cut_2, "goff");
     
     
     delete c1;
@@ -96,10 +101,14 @@ void due_grand()
     
     histo1->SetLineColor(kBlue);
     histo2->SetLineColor(kRed);
-    //TString title_x = var + "[rad]";
-    TString title_x = "p [GeV]";
+    histo1->SetFillColor(kBlue);
+    histo2->SetFillColor(kRed);
+    
+    TString title_x = var + "[" + um + "]";
+    //TString title_x = "clusterSecondMoment []";
     histo1->GetXaxis()->SetTitle(title_x);
     histo1->GetYaxis()->SetTitle("counts []");
+    
     
     TString title = "";
     histo1->SetTitle(title);
@@ -109,8 +118,8 @@ void due_grand()
     cout<<"NEntries_2 = "<<histo2->GetEntries()<<endl;
     
     TLegend *leg = new TLegend(0.6,0.6,0.78,0.78);
-    leg->AddEntry(histo1,"pRecoil REC ","l");
-    leg->AddEntry(histo2,"#bar{n} mc_P","l");
+    leg->AddEntry(histo1,"all #bar{n} candidates ","l");
+    leg->AddEntry(histo2,cut_2,"l");
     
     TCanvas *tela = new TCanvas("tela", "tela");
     
@@ -118,7 +127,7 @@ void due_grand()
     histo2->DrawCopy("HIST SAMES");
     leg->Draw("SAME");
     
-    TString title_out = "../images/pRecoil_nmcP_PDGsel.pdf";
+    TString title_out = "../images/cluster/" + var +"NHits.pdf";
     tela->SaveAs(title_out);
     
 }
@@ -146,7 +155,7 @@ void corr ()
     TTree *tree_1 = (TTree*)myf_1->Get("tree");
     
     TCanvas *c1 = new TCanvas("c1", "c1",800,600);
-    TString var = "vpho_r_pRecoil:nbar_mcP";
+    TString var = "vpho_r_pRecoil:nbar_p";
     TString um = "GeV";
     TString drawExpr1 = var + ">>histo1(100,0,7,100,0,7)";
     tree_1->Draw(drawExpr1,"nbar_mcPDG == -2112","colz");
@@ -160,12 +169,12 @@ void corr ()
     cout << "Correlation factor = " << rho << endl;
     
     TString title_x = var + " [" + um + "]";
-    histo1->GetXaxis()->SetTitle("#bar{n} mc_p [GeV]");
+    histo1->GetXaxis()->SetTitle("#bar{n} rec_p [GeV]");
     histo1->GetYaxis()->SetTitle("pRecoil [GeV]");
-    histo1->SetTitle("From generator");
-        
+    histo1->SetTitle("From reconstruction");
+    
     TLine *tl = new TLine(0,0,7,7);
-        
+    
     TCanvas *tela = new TCanvas("tela", "tela");
     
     TPaveText *pt = new TPaveText(0.15, 0.80, 0.45, 0.90, "NDC");
@@ -180,10 +189,108 @@ void corr ()
     tl->Draw("same");
     pt->Draw("same");
     //TString title_out = "../images/gen_" + var + ".pdf";
-    TString title_out = "../images/gen_mc_p_corr.pdf";
+    TString title_out = "../images/gen_rec_p_corr.pdf";
     tela->SaveAs(title_out);
     
     
 }
 
 
+
+void kin()
+{
+    gStyle->SetOptStat(1);
+    TString in_1, in_2;
+    TFile *myf_1 = nullptr;
+    TFile *myf_2 = nullptr;
+    
+    in_1 = "../../../vpho_std_isr_n_REC_merge100k_2.root";
+    in_2 = "../../../vpho_std_isr_n_REC_merge100k_3.root";
+    
+    ifstream in_f1(in_1);
+    ifstream in_f2(in_2);
+    
+    if(!in_f1 | !in_f2)
+    {
+        cout<<"Selected root file doesn't exist. Please, generate it. \n";
+        return;
+    }
+    in_f1.close();
+    in_f2.close();
+    
+    
+    myf_1 = new TFile(in_1);
+    myf_2 = new TFile(in_2);
+    
+    
+    TTree *tree_1 = (TTree*)myf_1->Get("tree");
+    TTree *tree_2 = (TTree*)myf_2->Get("tree");
+    
+    TCanvas *c1 = new TCanvas("c1", "c1",800,600);
+    
+    tree_1->Draw("vpho_r_pRecoilTheta-nbar_mcTheta>>histo1(100,-1,1)","nbar_mcPDG == -2112","goff");
+    tree_2->Draw("vpho_r_pRecoilTheta-nbar_mcTheta>>histo2(100,-1,1)","nbar_mcPDG == -2112","goff");
+    
+    delete c1;
+    
+    TH1 *histo1 = (TH1D*)gDirectory->Get("histo1");
+    TH1 *histo2 = (TH1D*)gDirectory->Get("histo2");
+    
+    double mean_1 = histo1->GetMean();
+    double devstd_1 = histo1->GetStdDev();
+    double entries_1 = histo1->GetEntries();
+    
+    double mean_2 = histo2->GetMean();
+    double devstd_2 = histo2->GetStdDev();
+    double entries_2 = histo2->GetEntries();
+    
+    
+    cout<<"entries_1 = "<<entries_1<<"|mean_1 = "<<mean_1<<"|devstd_1 = "<<devstd_1<<endl;
+    cout<<"entries_2 = "<<entries_2<<"|mean_2 = "<<mean_2<<"|devstd_2 = "<<devstd_2<<endl;
+    //histo1->Scale(1.0 / histo1->Integral());
+    //histo2->Scale(1.0 / histo2->Integral());
+    
+    double max1 = histo1->GetBinContent(histo1->GetMaximumBin());
+    cout<<"max1 = "<<max1<<endl;
+    double max2 = histo2->GetBinContent(histo2->GetMaximumBin());
+    cout<<"max2 = "<<max2<<endl;
+    double max = (max1 > max2) ? max1 : max2;
+    cout<<"max = "<<max<<endl;
+    
+    histo1->SetMaximum(max + 0.1*max);
+    histo2->SetMaximum(max + 0.1*max);
+    
+    histo1->SetLineColor(kBlue);
+    histo2->SetLineColor(kRed);
+    
+    histo1->GetXaxis()->SetTitle("#Delta#theta [rad]");
+    histo1->GetYaxis()->SetTitle("counts []");
+    
+    string title = "#Delta#theta comparison (n list)";
+    histo1->SetTitle(&title[0]);
+    
+    
+    cout<<"NEntries_1 = "<<histo1->GetEntries()<<endl;
+    cout<<"NEntries_2 = "<<histo2->GetEntries()<<endl;
+    
+    TLegend *leg = new TLegend(0.6,0.6,0.78,0.78);
+    leg->AddEntry(histo1,"no K.F. (histo1_copy)","l");
+    leg->AddEntry(histo2,"K.F. (histo2_copy)","l");
+    
+    TCanvas *tela = new TCanvas("tela", "tela");
+    
+    histo1->DrawCopy("HIST");
+    histo2->DrawCopy("HIST SAMES");
+    leg->Draw("SAME");
+    
+    tela->Update();
+    
+    tela->SaveAs("images/kin_comp_deltaTheta.pdf");
+    
+    
+    
+    
+    tela->SaveAs("images/isr_g_kin_comp_deltaTheta.pdf");
+    
+    
+}
